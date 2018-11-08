f00aa1378da22af264db5fbe470c590070e56c4f
nickycardenas <39623749+nickycardenas@users.noreply.github.com>
2018-11-08 15:54:17 -0400

    v

--- admin/docs/menu.php
+++ admin/docs/menu.php
@@ -25,12 +25,12 @@ if($_SESSION['usuario']==""){
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu -->
       <ul class="app-nav">
-          <li class="app-search">
+          <!--li class="app-search">
               <input class="app-search__input" type="search" placeholder="Buscar...">
               <button class="app-search__button"><i class="fa fa-search"></i></button>
-          </li>
+          </li-->
           <!--Notification Menu-->
-          <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
+          <!--li-- class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
               <ul class="app-notification dropdown-menu dropdown-menu-right">
                   <li class="app-notification__title">Notificaciones nuevas.</li>
                   <div class="app-notification__content">
@@ -69,7 +69,7 @@ if($_SESSION['usuario']==""){
                   </div>
                   <li class="app-notification__footer"><a href="#">See all notifications.</a></li>
               </ul>
-          </li>
+          </li-->
           <!-- User Menu-->
           <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i> &nbsp;<?php echo $_SESSION['rol']?></a>
               <ul class="dropdown-menu settings-menu dropdown-menu-right">
