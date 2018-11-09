de1613192952899733d9a3f069f7849c24282a47
haelx <alentandome@gmail.com>
2018-11-08 21:35:47 -0400

    no se que es

--- modelo/mdl_cuadernillo.php
+++ modelo/mdl_cuadernillo.php
@@ -25,15 +25,14 @@ class mdl_cuadernillo
     {
         date_default_timezone_set('America/Boa_Vista');
         $fecha= strftime("%Y-%m-%d");
-
-        $sql = "INSERT INTO cuadernillo (fecha_registro,fecha,decripcion) VALUE ('$fecha','$this->fecha','$this->descripcion');";
-        $this->obj_con->sin_retorno($sql);
-        $sql="select *from persona join estudiante e on persona.id_persona = e.persona_id_persona where id_persona=$_SESSION[id_persona]";
+        $sql="select * from persona join estudiante e on persona.id_persona = e.persona_id_persona where id_persona=$_SESSION[id_persona]";
         $datos=$this->obj_con->con_retorno($sql);
         $row=mysqli_fetch_assoc($datos);
-        $sql="select * from pasantia where estudiante_id_estudiante=$row[id_estudiante]";
-        $datos1=$this->obj_con->con_retorno($sql);
-
+        $sql="select * from pasantia where activopasantia=1 and estudiante_id_estudiante=$row[id_estudiante]";
+        $dat=$this->obj_con->con_retorno($sql);
+        $re=mysqli_fetch_assoc($dat);
+        $sql = "INSERT INTO cuadernillo (fecha_registro,fecha,decripcion,pasantia_id_pasantia) VALUE ('$fecha','$this->fecha','$this->descripcion',$re[id_pasantia]);";
+        $this->obj_con->sin_retorno($sql);
         echo $sql;
     }
 }
